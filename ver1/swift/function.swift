11da9c399f7c3726017964a93fd63cac6985ed81
Loren Pontiff <loren.pontiff@student.allenisd.org>
2022-05-09 09:20:24 -0500

    ISP Project (Please please work)

---
+++ ver1/swift/function.swift
@@ -0,0 +1,32 @@
+
+/*
+var stock1 = 50
+var stock2 = 50
+
+func refreshStocks(stock: Int) {
+    var stockValue = 0
+    if stock == 1 {
+        stockValue = stock1
+    }
+    if stock == 2 {
+        stockValue = stock2
+    }
+    let changeAmount = Int.random(in:1..<50)
+    let changeType = Int.random(in:1..<3)
+    if changeType == 1 {
+        print("Adding \(changeAmount)")
+        stockValue += changeAmount
+        print("New price: \(stockValue)")
+    }
+    if changeType == 2 {
+        print("Subtracting \(changeAmount)")
+        stockValue -= changeAmount
+        print("New price: \(stockValue)")
+    }
+    if changeType == 3 {
+        print("Pass.")
+    }
+}
+
+
+ */
